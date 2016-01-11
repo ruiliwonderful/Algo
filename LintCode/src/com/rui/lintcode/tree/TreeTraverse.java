18b33e4b34951e26a21147fe988f74a3acbfc667
rui <rui@ruis-MacBook-Pro.local>
2016-01-11 00:11:37 -0800

    update tree related problems

--- LintCode/src/com/rui/lintcode/tree/TreeTraverse.java
+++ LintCode/src/com/rui/lintcode/tree/TreeTraverse.java
@@ -1,7 +1,9 @@
 package com.rui.lintcode.tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 public class TreeTraverse {
@@ -64,6 +66,43 @@ public class TreeTraverse {
 		return null;
 
 	}
+	
+	 public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+	        
+	        if (root==null) return null;
+	        
+	        ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>();
+	        
+	        Queue<TreeNode> allNodesQueue=new LinkedList<TreeNode>();
+	       
+	        allNodesQueue.add(root);
+	        allNodesQueue.add(new TreeNode(Integer.MAX_VALUE));
+	        
+	         ArrayList<Integer> currLayer=new  ArrayList<Integer>();;
+	         
+	        while(!allNodesQueue.isEmpty()){
+	            TreeNode currTreeNode= allNodesQueue.poll();
+	           
+	            if(currTreeNode.val==Integer.MAX_VALUE) {
+	                if(currLayer!=null) result.add(currLayer);
+	               currLayer=new  ArrayList<Integer>();
+	               if(!allNodesQueue.isEmpty()) allNodesQueue.add(new TreeNode(Integer.MAX_VALUE));
+	            
+	        }
+	        else {
+	        	if(!allNodesQueue.isEmpty()){
+	            currLayer.add(currTreeNode.val);
+	            if(currTreeNode.left!=null)  allNodesQueue.add(currTreeNode.left);
+	            if(currTreeNode.right!=null) allNodesQueue.add(currTreeNode.right);
+	        }}
+	        
+	        
+	    }
+	    
+	    return result;
+	    
+	    
+	}
 
 	// http://www.lintcode.com/en/problem/construct-binary-tree-from-preorder-and-inorder-traversal/
 	public TreeNode buildTree(int[] preorder, int[] inorder) {
@@ -113,10 +152,12 @@ public class TreeTraverse {
 		while (currNode!=null) {
 			if (currNode.val == value) {
 				foundFlag = true;
+				if(root==currNode) leftFlag=true;
 				break;
 			} else if (currNode.val < value) {// go right
 				parentNode = currNode;
 				currNode = currNode.right;
+				leftFlag=false;
 			} else {
 				parentNode = currNode;
 				currNode = currNode.left;
@@ -241,25 +282,12 @@ public class TreeTraverse {
 	public static void main(String args[]){
 		TreeTraverse tt=new TreeTraverse();
 		
-		TreeNode t1root=new TreeNode(1);
-		TreeNode node1=new TreeNode(2);
-		TreeNode node2=new TreeNode(3);
+		TreeNode t1root=new TreeNode(5);
+		TreeNode node1=new TreeNode(1);
+		//TreeNode node2=new TreeNode(3);
+		t1root.left=node1;
 		
-		t1root.right=node1;
-		node1.right=node2;
-		
-		System.out.println(tt.serialize(t1root));
-     	String s1="1,#,2,#,3,#,#,";
-		System.out.println(tt.serialize(tt.dederialize(s1)));
-		System.out.println(s1.equals(tt.serialize(tt.dederialize(s1))));
-		String s2="123####";
-		System.out.println(tt.serialize(tt.dederialize(s2)));
-		System.out.println(s2.equals(tt.serialize(tt.dederialize(s2))));
-		String s3="12#3###";
-		System.out.println(tt.serialize(tt.dederialize(s3)));
-		System.out.println(s3.equals(tt.serialize(tt.dederialize(s3))));
-		String s4="1#2#3##";
-		System.out.println(tt.serialize(tt.dederialize(s4)));
-		System.out.println(s4.equals(tt.serialize(tt.dederialize(s4))));
+		tt.removeNodeFromBST(t1root, 5);
+	
 	}
 }
